30ee35c16b61a5a7350438f2469106544c72c250
Pengegg <49274893+Pengegg@users.noreply.github.com>
2020-01-31 22:35:49 +0800

    Setup Project
    -Setup demo scene for egg
    -Added rudimentary gameManager

---
+++ OvumER/Game/New Unity Project/Assets/Scripts/GameManagement/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public float demoVal = 10f;
+
+    private static GameManager _instance;
+    public static GameManager instance
+    {
+
+        get
+        {
+
+            return _instance;
+
+        }
+
+    }
+
+    void Awake()
+    {
+
+        if(_instance == null)
+        {
+
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+
+        }
+        else
+        {
+
+            if(instance != this)
+            Destroy(this.gameObject);
+
+        }
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
